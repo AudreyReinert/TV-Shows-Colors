416a705d261b961fb4178a064e60b2065b8498e6
AudreyReinert <reinertc@purdue.edu>
2020-05-04 09:30:19 -0400

    Added Base R Script

---
+++ colors.R
@@ -0,0 +1,46 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+require(scales)
+palletes<-list(
+  heyArnold=c("#E9D285", "#C54435","#3A669E", "#2C2C3C", "#527DCB"),
+  invaderZim=c("#D128AE", "#2B1454","#D2CDDA", "#5E9C4A", "#496E8C"),
+  angryBeavers=c("#AC5414", "#E3C337","#4E371B", "#932F1E", "#C96B35"),
+  Rocko=c("#E5C627","#F0D097","#DBCA68","#2D513B","#A73336"),
+  phineasAndFerb=c(),
+  adventureTime=c(),
+  stevenUniverse=c(),
+  avatar=c()
+
+
+
+
+)
+
+
+
